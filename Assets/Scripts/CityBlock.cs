26735fb02e299ab9d300a221921910303642751d
Hamdan Javeed <hamdan@hamdanjaveed.com>
2018-08-10 20:08:03 -0400

    Choose city blocks in pairs

---
+++ Assets/Scripts/CityBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBlock : MonoBehaviour {
+	private SpriteRenderer spriteR;
+
+	public void Choose() {
+		spriteR.color = new Color(0.5f, 0.2f, 0.7f);
+	}
+
+	void Start() {
+		spriteR = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+}
--- Assets/Scripts/CityManager.cs
+++ Assets/Scripts/CityManager.cs
@@ -9,9 +9,15 @@ public class CityManager : MonoBehaviour {
 
 	private const int roadSize = 4;
 
+	private const float blockChosenTimeThreshold = 5.0f; // Seconds between choosing a city block
+
 	[SerializeField] private GameObject cityBlockPrefab;
 
+
 	private GameObject[][] cityBlocks;
+	private bool[][] chosen;
+
+	private float timeSinceLastBlockChosen;
 
 	void Start () {
 		int citySizePx = (citySize * cityBlockSize) + ((citySize - 1) * roadSize);
@@ -29,5 +35,37 @@ public class CityManager : MonoBehaviour {
 				cityBlocks[x][y] = cityBlock;
 			}
 		}
+
+		timeSinceLastBlockChosen = Time.time;
+	}
+
+	void Update() {
+		if (Time.time - timeSinceLastBlockChosen > blockChosenTimeThreshold) {
+			ChooseRandomBlock();
+			timeSinceLastBlockChosen = Time.time;
+		}
+	}
+
+	private void ChooseRandomBlock() {
+		int tries = 0;
+		int dist = 0;
+		int cbx1, cby1;
+		int cbx2, cby2;
+		do {
+			cbx1 = Random.Range(0, citySize);
+			cby1 = Random.Range(0, citySize);
+			cbx2 = Random.Range(0, citySize);
+			cby2 = Random.Range(0, citySize);
+			dist = manhattanDistance(cbx1, cby1, cbx2, cby2);
+			if (dist == 0) continue; // Same block
+			tries++;
+		} while (tries < 5 && dist < 5);
+
+		cityBlocks[cbx1][cby1].GetComponent<CityBlock>().Choose();
+		cityBlocks[cbx2][cby2].GetComponent<CityBlock>().Choose();
+	}
+
+	private int manhattanDistance(int x1, int y1, int x2, int y2) {
+		return System.Math.Abs(x1 - x2) + System.Math.Abs(y1 - y2);
 	}
 }
